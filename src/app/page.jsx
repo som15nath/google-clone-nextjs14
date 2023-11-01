700b07093fc06d7545d4c833eb681dae4c55615e
somnath <mandal.somnath1234@gmail.com>
2023-11-01 12:08:09 +0530

    create search page for web and image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,7 +19,7 @@ export default function Home() {
       />
 
       <HomeSearch />
-      <Footer />
+    
     </div>
     </>
   )
---
+++ src/components/SearchHeader.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Searchheader() {
+  return (
+    <div>Searchheader</div>
+  )
+}
