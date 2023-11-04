371c07807c425b707c979baec678bac46006126c
somnath <mandal.somnath1234@gmail.com>
2023-11-04 12:41:37 +0530

    add fetch data from google search api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,18 @@
 
 
-export default function WebSearchPage() {
-  return (
-    <div>WebSearchPage</div>
-  )
-}
+export default async function WebSearchPage() {
+ 
+    const response = await fetch(
+      `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}s&
+      cx= ${process.env.CONTEXT_KEY}&q=lectures`
+    );
+
+    const data = await response.json();
+    console.log(data);
+
+    const results = data.items;
+  
+  return 
+    <>{results && results.map((result) =><h1>{result.title}</h1>)}</>;
+  
+};
