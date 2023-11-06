749245f7a93bd65d15840bba0a8edfb6a8cf6099
somnath <mandal.somnath1234@gmail.com>
2023-11-07 00:55:22 +0530

    add image result

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,7 +15,7 @@ export default function Home() {
       width="300"
       height="100"
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
-      />
+       alt='logo'/>
 
       <HomeSearch />
     
